ccaed0cb6995ec9bd243e5c618a4cf4c1a2a8aac
jaredwilli <jaredwilli@gmail.com>
2012-10-27 14:54:31 -0400

    works on ie9 and all panels with thumbs have gallerys and all load correctly now

--- js/main.js
+++ js/main.js
@@ -242,18 +242,7 @@ anti = {
 			}
 		},
 		showPanelContent: function(panelToShow) {
-			$('#'+ panelToShow).find('.panel-wrap').delay(1000).fadeIn(500);
-			
-			if ($('.gallery-thumbs a').length > 0) {
-				$('#default-nav li a').css({ opacity: 0.7 });
-				$('#default-nav li a').hover(function() {
-					$(this).stop().css({ opacity: 1 });
-				}, function() {
-					$(this).stop().css({ opacity: 0.7 });
-				});
-				
-				//$('.gallery').find
-			}
+			$('#'+ panelToShow).find('.panel-wrap').stop().delay(1000).fadeIn(500);
 		},
 		loadPanelContent: function(panelToLoad) {
 			return $.ajax({
@@ -288,30 +277,22 @@ anti = {
 				visibleSlide = 'hidden';
 
 			// Add current class to first thumbnail image
-			$(thumbs[0]).find('a').addClass('current');
+			//$(thumbs[0]).find('a').addClass('current');
 			
-			// Create click handler for thumbnails so they show larger versions correctly
-			thumbs.find('a').on('click', function(e) {
-				e.preventDefault();
-				
-				var slide = $(this).attr('href');
-				
-				// swap the states of the current thumbnail with clicked thumbnail 
-				thumbs.parents('ul').find('li a').removeClass('current').animate({ opacity: 0.7 }, 200);
-				$(this).addClass('current').animate({ opacity: 1 }, 200);
-
-				gallery.find('li').removeClass('active').fadeOut('slow');
-				$(slide).stop().addClass('active').fadeIn('slow');
-			});
 			// Loop over the thumbnails to generate the slides for each
 			for (var i = 0; i < thumbs.length; i++) {
-				var para = $(thumbs[i]).find('p').text(),
+				//if (i === 0) return;
+				
+				var para = $(thumbs[i]).find('p'),
 					href = $(thumbs[i]).find('a').attr('href').split('#')[1],
 					video = $(thumbs[i]).find('a').attr('data-vid');
-				//console.log(video);
+				
+				console.log(thumbs.length, thumbs[i], href);
 
-				if (i === 0) {
+				if (href === 'slide-1') {
 					visibleSlide = 'active';
+				} else {
+					visibleSlide = '';
 				}
 
 				galleryStr += '<li id="'+ href +'" class="'+ visibleSlide +'">';
@@ -323,19 +304,35 @@ anti = {
 					galleryStr += '<img src="panels/'+ panel +'/'+ href +'.jpg" alt="'+ panel +' '+ href +'" />';
 				}
 
-				galleryStr += '<p>'+ para +'</p></li>';
+				galleryStr += '<p>'+ para.text() +'</p></li>';
+				para.remove();
 			}
 			galleryStr += '</ul>';
 			gallery.append(galleryStr);
 
-			$(thumbs[i]).on(function(e) {
+			if ($('#default-nav li a').length > 0) {
+				$('#default-nav li a').css({ opacity: 0.7 });
+				$('#default-nav li a').hover(function() {
+					$(this).stop().css({ opacity: 1 });
+				}, function() {
+					$(this).stop().css({ opacity: 0.7 });
+				});
+			}
+			
+			// Create click handler for thumbnails so they show larger versions correctly
+			thumbs.find('a').on('click', function(e) {
 				e.preventDefault();
-				e.stopPropogatoin();
-
+				
+				// use href of thumbnail clicked to find slide ID to display
 				var slide = $(this).attr('href');
-
-				$('#'+ panel).find('.gallery li').hide();
-				$('#'+ panel +' .gallery').find(slide).show();
+				
+				// swap the active state of the current thumbnail with clicked thumbnail
+				thumbs.find('a').removeClass('current').animate({ opacity: 0.7 }, 200);
+				$(this).addClass('current').animate({ opacity: 1 }, 200);
+				
+				// hide all slides before showing slide for clicked thumbnail
+				gallery.find('li').removeClass('active').fadeOut(200);
+				gallery.find(slide).addClass('active').fadeIn(200);
 			});
 		}
 	},
