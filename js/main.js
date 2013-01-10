6b4a90e1547d10923f46df63f3171bff60360c40
jaredwilli <jaredwilli@gmail.com>
2013-01-10 09:15:46 -0500

    fix broken things add stuff idk

--- js/main.js
+++ js/main.js
@@ -91,6 +91,9 @@ anti = {
 			window.addEventListener('resize', anti.utils.resizeToScreen, false);
 		}
 		anti.utils.resizeToScreen();
+
+		anti.time();
+		//anti.canvas.init();
 	},
 
 	panelNavigation: {
@@ -306,7 +309,7 @@ anti = {
 			//$(thumbs[0]).find('a').addClass('current');
 
 			// Loop over the thumbnails to generate the slides for each
-			for (var i = 0; i < thumbs.length; i++) {
+			for (var i = 1; i < thumbs.length; i++) {
 				//if (i === 0) return;
 
 				var para = $(thumbs[i]).find('p'),
@@ -362,6 +365,57 @@ anti = {
 			});
 		}
 	},
+
+	time: function() {
+		// Change background depending on user's time
+		var d = new Date(),
+			n = d.getHours();
+
+		if (n > 19) {
+			// If time is 7PM or later apply night theme to body
+			$('body').addClass('night').css({ background: 'rgba(1, 1, 2, 0.7)' });
+
+		}
+		else if (n > 16 && n < 19) {
+			// If time is between 4PM – 7PM sunset theme to body
+			$('body').addClass('sunset').css ({ background: 'rgba(208, 58, 236, 0.7)' });
+		}
+		else {
+			// Otherwise use day theme
+			$('body').addClass('day');
+		}
+	},
+
+	canvas: {
+		canvas: null,
+		ctx: null,
+
+		init: function() {
+			console.log('Canvas init');
+
+			this.canvas = document.getElementById('bgGame');
+			this.ctx = this.canvas.getContext('2d');
+			var winWidth = window.innerWidth,
+				winHeight = window.innerHeight;
+
+			this.canvas.width = winWidth;
+			this.canvas.height = winHeight;
+
+			this.draw();
+		},
+		draw: function() {
+			console.log(this);
+
+			this.ctx.beginPath();
+				this.ctx.fill();
+
+		},
+		update: function() {
+			console.log(this);
+
+		}
+	},
+
 	utils: {
 		resizeToScreen: function() {
 			anti.layers.layer3.pos[2] = window.innerWidth -(window.innerWidth * 0.20);
@@ -479,3 +533,172 @@ UTIL = {
 };
 //kick it all off here
 $(document).ready(UTIL.loadEvents);
+
+
+
+// /* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// /* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// /* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// 	var reqAnimFrame = window.requestAnimationFrame 	  ||
+// 					   window.webkitRequestAnimationFrame ||
+// 					   window.mozRequestAnimationFrame 	  ||
+// 					   function(func) { setTimeout(func, 17); };
+
+// 	var html = document.documentElement;
+
+// 	function Flake() {
+// 		this.reset(maxX);
+// 	}
+
+// 	Flake.prototype = {
+// 		tick: function() {
+// 			var sidePhase = this.sidePhase = this.sideVel;
+// 			this.x += this.sideVel;
+// 			this.y += this.vel;
+// 		},
+// 		reset: function(maxX) {
+// 			var rand = Math.random(),
+// 				chanceOfLargeFlake = 0.15;
+
+// 			this.size = 2 + rand * 5;
+// 			this.vel = 3 + rand * 3;
+// 			this.alpha = 0.5 + rand * 0.8;
+
+// 			// random x position
+// 			this.x = rand * maxX;
+// 			this.y = -this.size;
+
+// 			// side to side movement
+// 			this.sideVel = (0.5 - rand) * this.vel;
+
+// 			return this;
+// 		}
+// 	};
+
+
+
+//   (function() {
+//     var canvas = document.createElement('canvas');
+//     var context = canvas.getContext('2d');
+//     var settleCanvas = document.createElement('canvas');
+//     var settleContext = context && settleCanvas.getContext('2d');
+//     var canvasStyle = canvas.style;
+//     var settleCanvasStyle = settleCanvas.style;
+//     var windowResized;
+//     var activeFlakes = [];
+//     var snowflakesPerPixelPerSecond = 0.05;
+//     var PIx2 = Math.PI*2;
+//     var assumedFps = 60;
+//     var settlePoint;
+//     var snowflakePool = [];
+
+//     function resizeCanvas() {
+//       settlePoint = Array(html.clientWidth);
+//       settleCanvas.width = canvas.width = html.clientWidth;
+//       settleCanvas.height = canvas.height = html.clientHeight;
+//     }
+
+//     function updateSettlePoints(flake) {
+//       var size = flake.size * 0.8; // reduce coral effect
+//       var xStart = Math.floor(flake.x - size);
+//       var range = size * 2;
+//       var newY;
+
+//       if (xStart < 0) {
+//         range += xStart;
+//         xStart = 0;
+//       }
+//       if (xStart + range > settlePoint.length) {
+//         range -= xStart + range - settlePoint.length;
+//       }
+
+//       for (var i = 0; i < range; i++) {
+//         newY = flake.y - (size * Math.cos( (i/range) * Math.PI - (Math.PI/2) ));
+//         settlePoint[i + xStart] = Math.min(settlePoint[i + xStart] || Infinity, newY);
+//       }
+//     }
+
+//     var flakesToCreate = 0;
+//     function frame() {
+//       flakesToCreate += (snowflakesPerPixelPerSecond / assumedFps) * canvas.width;
+//       var flakesThisFrame = Math.floor(flakesToCreate);
+//       flakesToCreate -= flakesThisFrame;
+
+//       // clear canvas
+//       if (windowResized) {
+//         resizeCanvas();
+//         windowResized = false;
+//       }
+//       else {
+//         context.clearRect(0, 0, canvas.width, canvas.height);
+//       }
+
+//       // add new flake?
+//       while ( flakesThisFrame-- ) {
+//         if (snowflakePool.length) {
+//           activeFlakes.push( snowflakePool.pop().reset(canvas.width) );
+//         }
+//         else {
+//           activeFlakes.push( new Snowflake(canvas.width) );
+//         }
+//       }
+
+//       var i = activeFlakes.length;
+//       var flake;
+
+//       // for each flake...
+//       while (i--) {
+//         flake = activeFlakes[i];
+//         flake.tick();
+
+//         // splice flake if it's now out of rendering zone
+//         if (flake.y >= canvas.height || flake.y >= settlePoint[Math.floor(flake.x)]) {
+//           snowflakePool.push.apply(snowflakePool, activeFlakes.splice(i, 1));
+//           // this flake effects our settle points
+//           if (flake.alpha > 0.8) {
+//             updateSettlePoints(flake);
+//           }
+//           settleContext.fillStyle='rgba(255, 255, 255, ' + flake.alpha + ')';
+//           settleContext.beginPath();
+//           settleContext.arc(flake.x, flake.y, flake.size, 0, PIx2, true);
+//           settleContext.closePath();
+//           settleContext.fill();
+//           continue;
+//         }
+
+//         // render to canvas
+//         context.fillStyle='rgba(255, 255, 255, ' + flake.alpha + ')';
+//         context.beginPath();
+//         context.arc(flake.x, flake.y, flake.size, 0, PIx2, true);
+//         context.closePath();
+//         context.fill();
+//       }
+//       requestAnimationFrame(frame);
+//     }
+
+//     if (context) {
+//       resizeCanvas();
+
+//       // style the canvas
+//       canvasStyle.position = 'fixed';
+//       canvasStyle.top = 0;
+//       canvasStyle.left = 0;
+//       canvasStyle.zIndex = 1138;
+//       canvasStyle['pointerEvents'] = 'none';
+
+//       settleCanvasStyle.cssText = canvasStyle.cssText;
+
+//       // watch out for resizes
+//       window.addEventListener('resize', function() {
+//         windowResized = true;
+//       }, false);
+
+//       // add it to the page & start animating
+//       document.body.appendChild(canvas);
+//       document.body.appendChild(settleCanvas);
+//       requestAnimationFrame(frame);
+//     }
+//   })();
+
+// })();
